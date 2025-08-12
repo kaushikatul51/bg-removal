f9c1916deb38de0d839018d5b2458a0dee88cf2b
Atul Kaushik <kaushikatul51@gmail.com>
2025-08-12 23:34:20 +0530

    second commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+
+//App config
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+//Intialize middlewares
+app.use(express.json())
+app.use(cors())
+
+//Api routes 
+app.get('/',(req,res)=> res.send("API Working"))
+
+app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`))
